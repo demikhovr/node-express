9f7aa1b0e03d08451bc92ccc8101374137a37e97
demikhovr <demikhovr@gmail.com>
2019-09-29 22:50:18 +0300

    use handlebars to render views

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
