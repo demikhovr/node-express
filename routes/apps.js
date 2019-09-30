3232ba8316c698671261ae26cb0ef6586ceab387
demikhovr <demikhovr@gmail.com>
2019-09-30 23:51:29 +0300

    render apps

--- routes/apps.js
+++ routes/apps.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
+const App = require('../models/app');
 
 const router = Router();
-router.get('/', (req, res) => res.render('apps', {
-  title: 'Apps',
-  isApps: true,
-}));
+router.get('/', async (req, res) => {
+  const apps = await App.getAll();
+
+  res.render('apps', {
+    title: 'Apps',
+    isApps: true,
+    apps,
+  });
+});
 
 module.exports = router;
