5fc2699cb8a755509091e4145b9282b67b21f3ff
demikhovr <demikhovr@gmail.com>
2019-10-02 22:04:28 +0300

    backend: removing card

--- models/cart.js
+++ models/cart.js
@@ -34,6 +34,29 @@ class Cart {
     });
   }
 
+  static async remove(id) {
+    const cart = await Cart.fetch();
+    const index = cart.apps.findIndex(it => it.id === id);
+    const app = cart.apps[index];
+
+    if (app.count === 1) {
+      cart.apps.filter(it => it.id !== id);
+    } else {
+      cart.apps[index].count -= 1;
+    }
+
+    cart.price -= app.price;
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(cart);
+        }
+      });
+    });
+  }
+
   static async fetch() {
     return new Promise((resolve, reject) => {
       fs.readFile(p, 'utf-8', (err, content) => {
