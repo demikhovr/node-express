ccdc687cb821e7d425d3de0668eebde4aa03f3e3
demikhovr <demikhovr@gmail.com>
2019-10-02 22:28:17 +0300

    frontend: removing card

--- public/app.js
+++ public/app.js
@@ -1,9 +1,39 @@
+const toCurrency = price => new Intl.NumberFormat('ru-RU', {
+  currency: 'rub',
+  style: 'currency',
+}).format(price);
+
 const prices = document.querySelectorAll('.price');
 prices.forEach((node) => {
-  const price = new Intl.NumberFormat('ru-RU', {
-    currency: 'rub',
-    style: 'currency',
-  }).format(node.textContent);
-
-  node.textContent = price;
+  node.textContent = toCurrency(node.textContent);
 });
+
+const $cart = document.querySelector('.cart');
+
+if ($cart) {
+  $cart.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('js-remove')) {
+      const { id } = evt.target.dataset;
+
+      fetch(`/cart/remove/${id}`, {
+        method: 'delete',
+      })
+        .then(res => res.json())
+        .then((cart) => {
+          if (cart.apps.length) {
+            $cart.querySelector('tbody').innerHTML = cart.apps.map(it => (`
+              <tr>
+                <td>${it.title}</td>
+                <td>${it.count}</td>
+                <td>
+                  <button class="btn btn-small js-remove" type="button" data-id="${it.id}">Delete</button>
+                </td>
+              </tr>`)).join('');
+            $cart.querySelector('.price').textContent = toCurrency(cart.price);
+          } else {
+            $cart.innerHTML = '<p>Cart is empty</p>';
+          }
+        })
+    }
+  });
+}
